f38c6d11a23d92c71093f6875243197234d507a2
n3m3sis42 <n3m3sis42@gmail.com>
2017-11-05 16:05:50 -0500

    styling changes

--- js/index.js
+++ js/index.js
@@ -1,36 +1,17 @@
-var screams = 0;
-var volume = 30;
-var bondIntegrity = 100;
+var loc = "???"
 
-// 30 db - whisper/croak
-// 88-90 db - scream
-// 120 db - loudest scream on record
-// 70 db - normal voice
+function setLocation() {
 
-function getScreamVolume() {
-  switch (true) {
-    case screams === 0:
-      return 30;
-    case screams < 20:
-      return randomIntBetween(88, 91);
-    case screams < 100:
-      return randomIntBetween(88, 110);
-    case screams < 200:
-        return randomIntBetween(100, 115);      
-    default:
-      return randomIntBetween(88, 91);
-  }
 }
 
-function screamClick(number) {
-    volume = getScreamVolume();
-    screams += number;
-    document.getElementById("screams").innerHTML = screams;
-    document.getElementById("volume").innerHTML = volume;
-    // NOTE make volume go to zero in between button clicks
-    // also after scream volume reaches a certain point, screaming becomes automatic
-};
+function logEntry(text) {
+  return (
+    `<div class="right-tab-item">
+      ${text}
+    </div>`
+  )
+}
 
-function struggleClick(number) {
-  // NOTE if you've screamed enough to start losing your voice, you have to struggle less to loosen your bonds
-};
+function writeToLog(text) {
+    // document.getElementById("game-log").innerHTML
+}
