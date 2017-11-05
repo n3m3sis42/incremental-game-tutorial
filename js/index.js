3fb9eeb903e913b8f06f51728cbe3569e9b47ea5
n3m3sis42 <n3m3sis42@gmail.com>
2017-11-05 14:27:16 -0500

    playing with scream button functionality and also hid some buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,36 @@
 var screams = 0;
+var volume = 30;
+var bondIntegrity = 100;
+
+// 30 db - whisper/croak
+// 88-90 db - scream
+// 120 db - loudest scream on record
+// 70 db - normal voice
+
+function getScreamVolume() {
+  switch (true) {
+    case screams === 0:
+      return 30;
+    case screams < 20:
+      return randomIntBetween(88, 91);
+    case screams < 100:
+      return randomIntBetween(88, 110);
+    case screams < 200:
+        return randomIntBetween(100, 115);      
+    default:
+      return randomIntBetween(88, 91);
+  }
+}
 
 function screamClick(number) {
+    volume = getScreamVolume();
     screams += number;
     document.getElementById("screams").innerHTML = screams;
+    document.getElementById("volume").innerHTML = volume;
+    // NOTE make volume go to zero in between button clicks
+    // also after scream volume reaches a certain point, screaming becomes automatic
+};
+
+function struggleClick(number) {
+  // NOTE if you've screamed enough to start losing your voice, you have to struggle less to loosen your bonds
 };
